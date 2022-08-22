188443da6d24c17b734f98dfac550da46f669d35
wzmw-zr <1767153298@qq.com>
2022-08-22 20:11:20 +0800

    support anomal dataset

--- unknown_recognition/datasets/__init__.py
+++ unknown_recognition/datasets/__init__.py
@@ -3,8 +3,10 @@ from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
 from .custom import CustomDataset
 from .dataset_wrappers import (ConcatDataset, MultiImageMixDataset,
                                RepeatDataset)
+from .anomal_dataset import AnomalDataset
 
 __all__ = [
     'CustomDataset', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
     'DATASETS', 'build_dataset', 'PIPELINES', 'MultiImageMixDataset',
+    'AnomalDataset'
 ]
