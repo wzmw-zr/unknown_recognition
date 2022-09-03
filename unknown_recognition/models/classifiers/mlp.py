8c77f817ac6731f0d5a179b1d78c6d6ab7370231
wzmw-zr <1767153298@qq.com>
2022-09-03 09:54:10 +0800

    add Normalization Layer

--- unknown_recognition/models/classifiers/mlp.py
+++ unknown_recognition/models/classifiers/mlp.py
@@ -13,6 +13,18 @@ from ..builder import CLASSIFIER, build_loss
 from ..losses import accuracy
 
 
+def get_norm_layer(norm_type: str, in_channels: int):
+    if norm_type == "BN":
+        return nn.BatchNorm2d(in_channels)
+    elif norm_type == "LN":
+        return nn.GroupNorm(1, in_channels)
+    elif norm_type == "IN":
+        return nn.GroupNorm(in_channels, in_channels)
+    elif norm_type is None:
+        return nn.Identity()
+    raise NotImplementedError
+
+
 @CLASSIFIER.register_module()
 class MLP(BaseModule):
     def __init__(
@@ -35,7 +47,8 @@ class MLP(BaseModule):
             loss_weight=1.0),
         ignore_index=255,
         # sampler=None,
-        init_cfg=None
+        init_cfg=None,
+        norm=None
     ):
         super(MLP, self).__init__(init_cfg)
         self.input_features = input_features_infos
@@ -45,6 +58,7 @@ class MLP(BaseModule):
         self.hidden_channels = hidden_channels
         self.num_classes = num_classes
         self.ignore_index = ignore_index
+        self.norm = norm
 
         if isinstance(loss_decode, dict):
             self.loss_decode = build_loss(loss_decode)
@@ -57,7 +71,9 @@ class MLP(BaseModule):
                 but got {type(loss_decode)}')
 
         self.fc1 = nn.Conv2d(self.in_channels, hidden_channels, 1)
+        self.norm_layer_1 = get_norm_layer(self.norm, hidden_channels)
         self.fc2 = nn.Conv2d(hidden_channels, hidden_channels, 1)
+        self.norm_layer_2 = get_norm_layer(self.norm, hidden_channels)
         self.fc3 = nn.Conv2d(hidden_channels, self.num_classes, 1)
 
     def init_weights(self):
@@ -79,8 +95,12 @@ class MLP(BaseModule):
         assert inputs.shape[1] == self.in_channels, \
             f"input channels should be {self.in_channels}, but get {inputs.shape[1]}"
 
-        output = F.relu(self.fc1(inputs))
-        output = F.relu(self.fc2(output))
+        output = self.fc1(inputs)
+        output = self.norm_layer_1(output)
+        output = F.relu(output)
+        output = self.fc2(output)
+        output = self.norm_layer_2(output)
+        output = F.relu(output)
         output = self.fc3(output)
         return output
 
