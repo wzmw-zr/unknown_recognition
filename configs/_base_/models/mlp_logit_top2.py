cb1ebec3b56a4f5d0312c2f222d4b0a078652ad4
wzmw-zr <1767153298@qq.com>
2022-09-05 09:38:34 +0800

    add mlp with new input feature

---
+++ configs/_base_/models/mlp_logit_top2.py
@@ -0,0 +1,30 @@
+# model settings
+# norm_cfg = dict(type='SyncBN', requires_grad=True)
+model = dict(
+    type='UnknownDetectorLogitTop2',
+    pretrained=None,
+    classifier=dict(
+        type="MLP",
+        input_features_infos=[
+            dict(
+                type="logit",
+                in_channels=19,
+            ),
+            dict(
+                type="top2_distance",
+                in_channels=1,
+            )
+        ],
+        hidden_channels=256,
+        num_classes=2,
+        loss_decode=dict(
+            type='CrossEntropyLoss',
+            use_sigmoid=False,
+            loss_weight=1.0),
+        ignore_index=255,
+        init_cfg=None,
+        norm=None
+    ),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
