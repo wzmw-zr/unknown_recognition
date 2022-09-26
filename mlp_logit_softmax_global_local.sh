8f847d66ab6bf20dbf9576c1709053ffd6d506f2
wzmw-zr <1767153298@qq.com>
2022-09-26 22:19:50 +0800

    update train script for parameter tuning

--- mlp_logit_softmax_global_local.sh
+++ mlp_logit_softmax_global_local.sh
@@ -4,43 +4,80 @@ then
   mkdir results
 fi
 
-for lr_update in CosineAnnealing
-do
-  logit_norm_types=(local global)
-  lrs=(0.1)
-  epochs=(300)
+learning_rate=$1
+alpha=$2
+beta=$3
 
-  unknown_ratio=0.1
-  known_ratio=0.9
+logit_norm_types=(global local)
+norm_types=(LN BN None)
+# lrs=(0.00001 0.0001 0.001 0.01)
+# lrs=(0.1 0.07 0.05 0.02 0.01 0.007 0.005 0.002 0.001 0.0007 0.0005 0.0002 0.0001)
+lrs=($learning_rate)
+# epochs=(20 50 100)
+epochs=(100)
+
+# for lr_update in CosineAnnealing poly
+for lr_update in fixed
+do
   for lr in ${lrs[*]}
   do
-    for epoch in ${epochs[*]}
+    for norm_type in ${norm_types[*]}
     do
-      if [[ ! -d results/mlp_softmax ]]
-      then
-        mkdir results/mlp_softmax
-      fi
-
-      python train_single_gpu/mlp_softmax_300_epoch_logit_adjust_loss.py ${lr} ${epoch} ${lr_update} ${unknown_ratio} ${known_ratio} > results/mlp_softmax/mlp_softmax_with_logit_adjust_loss_lr_${lr}_epoch_${epoch}_${lr_update}_${unknown_ratio}_${known_ratio} 2>&1
-
-      for logit_norm_type in ${logit_norm_types[*]}
+      for epoch in ${epochs[*]}
       do
-        if [[ ! -d results/mlp_logit_${logit_norm_type} ]]
+        if [[ ! -d results/mlp_softmax ]]
         then
-          mkdir results/mlp_logit_${logit_norm_type}
+          mkdir results/mlp_softmax
         fi
+        python train_single_gpu/mlp_softmax_tversky_loss.py\
+        ${lr} ${epoch} ${lr_update} ${norm_type} ${alpha} ${beta}\
+        > results/mlp_softmax/mlp_softmax_lr_${lr}_epoch_${epoch}_${lr_update}_${norm_type}_${alpha}_${beta} 2>&1
 
-        python train_single_gpu/mlp_logit_300_epoch_logit_adjust_loss.py ${lr} ${epoch} ${lr_update} ${unknown_ratio} ${known_ratio} ${logit_norm_type} > results/mlp_logit_${logit_norm_type}/mlp_logit_with_logit_adjust_loss_lr_${lr}_epoch_${epoch}_${lr_update}_${unknown_ratio}_${known_ratio}_${logit_norm_type} 2>&1
+        # if [[ ! -d results/mlp_softmax_distance ]]
+        # then
+        #   mkdir results/mlp_softmax_distance
+        # fi
+        # python train_single_gpu/mlp_softmax_distance_tversky_loss.py\
+        # ${lr} ${epoch} ${lr_update} ${norm_type} ${alpha} ${beta}\
+        # > results/mlp_softmax_distance/mlp_softmax_distance_lr_${lr}_epoch_${epoch}_${lr_update}_${norm_type}_${alpha}_${beta} 2>&1
 
-        if [[ ! -d results/mlp_logit_with_softmax_${logit_norm_type} ]]
-        then
-          mkdir results/mlp_logit_with_softmax_${logit_norm_type}
-        fi
+        for logit_norm_type in ${logit_norm_types[*]}
+        do
+          if [[ ! -d results/mlp_logit ]]
+          then
+            mkdir results/mlp_logit
+          fi
+          python train_single_gpu/mlp_logit_tversky_loss.py\
+          ${lr} ${epoch} ${lr_update} ${logit_norm_type} ${norm_type} ${alpha} ${beta}\
+          > results/mlp_logit/mlp_logit_lr_${lr}_epoch_${epoch}_${lr_update}_${logit_norm_type}_${norm_type}_${alpha}_${beta} 2>&1
 
-        python train_single_gpu/mlp_logit_adjust_loss_300_epoch.py ${lr} ${epoch} ${lr_update} ${unknown_ratio} ${known_ratio} ${logit_norm_type} > results/mlp_logit_with_softmax_${logit_norm_type}/mlp_logit_adjust_loss_lr_${lr}_epoch_${epoch}_${lr_update}_${unknown_ratio}_${known_ratio}_${logit_norm_type} 2>&1
+          if [[ ! -d results/mlp_logit_softmax ]]
+          then
+            mkdir results/mlp_logit_softmax 
+          fi
+          python train_single_gpu/mlp_logit_with_softmax_tversky_loss.py\
+          ${lr} ${epoch} ${lr_update} ${logit_norm_type} ${norm_type} ${alpha} ${beta}\
+          > results/mlp_logit_softmax/mlp_logit_softmax_lr_${lr}_epoch_${epoch}_${lr_update}_${logit_norm_type}_${norm_type}_${alpha}_${beta} 2>&1
+
+          # if [[ ! -d results/mlp_max_logit ]]
+          # then
+          #   mkdir results/mlp_max_logit
+          # fi
+          # python train_single_gpu/mlp_max_logit_tversky_loss.py\
+          # ${lr} ${epoch} ${lr_update} ${logit_norm_type} ${norm_type} ${alpha} ${beta}\
+          # > results/mlp_max_logit/mlp_max_logit_lr_${lr}_epoch_${epoch}_${lr_update}_${logit_norm_type}_${norm_type}_${alpha}_${beta} 2>&1
+
+          # if [[ ! -d results/mlp_max_logit_softmax_distance ]]
+          # then
+          #   mkdir results/mlp_max_logit_softmax_distance
+          # fi
+          # python train_single_gpu/mlp_max_logit_with_softmax_distance_tversky_loss.py\
+          # ${lr} ${epoch} ${lr_update} ${logit_norm_type} ${norm_type} ${alpha} ${beta}\
+          # > results/mlp_max_logit_softmax_distance/mlp_max_logit_softmax_distance_lr_${lr}_epoch_${epoch}_${lr_update}_${logit_norm_type}_${norm_type}_${alpha}_${beta} 2>&1
+
+        done
       done
     done
   done
-
 done
 
